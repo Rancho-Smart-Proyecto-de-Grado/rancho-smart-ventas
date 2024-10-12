b7a7d33951b577e71cbae0035efa4d9efeb2fc95
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:49:28 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.A

---
+++ src/main/java/com/rancho_smart/ventas_composer/VentasComposerApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ventas_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasComposerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/ventas_composer/VentasComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ventas_composer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
